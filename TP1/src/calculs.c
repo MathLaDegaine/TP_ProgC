798e39510e075f55613bab59a67fe714187463b8
MathLaDegaine <143786511+MathLaDegaine@users.noreply.github.com>
2025-04-04 11:37:44 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10, num2 = 4;
+    char op = '+';
+
+    switch (op) {
+        case '+': printf("%d + %d = %d\n", num1, num2, num1 + num2); break;
+        case '-': printf("%d - %d = %d\n", num1, num2, num1 - num2); break;
+        case '*': printf("%d * %d = %d\n", num1, num2, num1 * num2); break;
+        case '/': 
+            if (num2 != 0)
+                printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            else
+                printf("Division par zéro!\n");
+            break;
+        case '%': printf("%d %% %d = %d\n", num1, num2, num1 % num2); break;
+        case '&': printf("%d & %d = %d\n", num1, num2, num1 & num2); break;
+        case '|': printf("%d | %d = %d\n", num1, num2, num1 | num2); break;
+        case '~': printf("~%d = %d\n", num1, ~num1); break;
+        default: printf("Opérateur inconnu\n");
+    }
+
+    return 0;
+}
 
