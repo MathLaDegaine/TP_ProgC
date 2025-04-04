fa122f23a81a6ad21d6bc6615fb2e5c703ee44b9
MathLaDegaine <143786511+MathLaDegaine@users.noreply.github.com>
2025-04-04 12:20:40 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+        // espaces
+        for (int s = 1; s <= n - i; s++)
+            printf(" ");
+
+        // chiffres croissants
+        for (int j = 1; j <= i; j++)
+            printf("%d", j);
+
+        // chiffres décroissants
+        for (int j = i - 1; j >= 1; j--)
+            printf("%d", j);
+
+        printf("\n");
+    }
+
+    printf("Pyramide terminée.\n");
+    return 0;
+}
 
